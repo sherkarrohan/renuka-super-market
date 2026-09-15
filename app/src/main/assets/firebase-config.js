fbc6aac1cd46dd73af149c906c77e39d6ec8bb68
Rohan Sherkar <rohansherkar2207@gmail.com>
2026-09-15 04:27:30 +0000

    Add Firebase cloud sync

---
+++ app/src/main/assets/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDDukS8ZSgER7PV_wKAUGTJiJE2X1BHV04",
+  authDomain: "renuka-super-market.firebaseapp.com",
+  projectId: "renuka-super-market",
+  storageBucket: "renuka-super-market.firebasestorage.app",
+  messagingSenderId: "66043492851",
+  appId: "1:66043492851:web:97176ec3343f7b59bd7a86"
+};
